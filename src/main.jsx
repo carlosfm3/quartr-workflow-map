f13ff3bbe520256bb4fd022a9d8c15bdcd524c40
Carlos Meneses <carlosfm@Carloss-MacBook-Air.local>
2026-04-27 13:57:53 +0200

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './styles.css'
 import WorkflowMap from './WorkflowMap.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <WorkflowMap />
+    <Analytics />
   </React.StrictMode>
 )
